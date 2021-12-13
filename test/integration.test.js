b79454277327db122695315a59009341b14df0c8
Radomir Drndarski <radomir.drndarski@nearform.com>
2021-12-13 15:24:56 +0100

    Added integration test for v2

--- test/integration.test.js
+++ test/integration.test.js
@@ -10,22 +10,45 @@ const FastifySecretsHashiCorp = require('..')
 const SECRET_KEY = 'exampleKey'
 const SECRET_NAME = uuid.v4()
 const SECRET_CONTENT = uuid.v4()
-const MOUNT_POINT = 'fastify-integration-test'
+const MOUNT_POINT = {
+  V1: 'fastify-integration-test_v1',
+  V2: 'fastify-integration-test_v2'
+}
+const VERSIONS = {
+  V1: 1,
+  V2: 2
+}
 const vault = nodeVault()
 
-async function setup() {
-  await vault.mount({ mount_point: MOUNT_POINT, type: 'kv', options: { version: 1 } })
-  await vault.write(`${MOUNT_POINT}/${SECRET_NAME}`, { [SECRET_KEY]: SECRET_CONTENT })
+async function setup(version) {
+  await vault.mount({ mount_point: MOUNT_POINT[version], type: 'kv', options: { version: VERSIONS[version] } })
+
+  let path = `${MOUNT_POINT[version]}/${SECRET_NAME}`
+  let data = { [SECRET_KEY]: SECRET_CONTENT }
+  
+  if (version === 'V2') {
+    path = `${MOUNT_POINT[version]}/data/${SECRET_NAME}`
+    data = { data: { [SECRET_KEY]: SECRET_CONTENT } }
+  }
+
+  await vault.write(path, data)
+}
+
+async function cleanup(version) {
+  const path =
+    version === 'V1' ? `${MOUNT_POINT[version]}/${SECRET_NAME}` : `${MOUNT_POINT[version]}/metadata/${SECRET_NAME}`
+  await vault.delete(path)
+  await vault.unmount({ mount_point: MOUNT_POINT[version] })
 }
 
 teardown(async () => {
-  await vault.delete(`${MOUNT_POINT}/${SECRET_NAME}`)
-  await vault.unmount({ mount_point: MOUNT_POINT })
+  await cleanup('V1')
+  await cleanup('V2')
 })
 
-test('integration', async (t) => {
+test('integration v1', async (t) => {
   t.plan(1)
-  await setup()
+  await setup('V1')
 
   const fastify = Fastify({
     logger: process.env.TEST_LOGGER || false
@@ -42,8 +65,42 @@ test('integration', async (t) => {
       vaultOptions: {
         endpoint: 'http://127.0.0.1:8200'
       },
-      mountPoint: MOUNT_POINT,
-      useKVv1: true,
+      mountPoint: MOUNT_POINT.V1,
+      useKVv1: true
+    }
+  })
+
+  await fastify.ready()
+
+  t.same(
+    fastify.secrets,
+    {
+      test: SECRET_CONTENT
+    },
+    'decorates fastify with secret content'
+  )
+})
+
+test('integration v2', async (t) => {
+  t.plan(1)
+  await setup('V2')
+
+  const fastify = Fastify({
+    logger: process.env.TEST_LOGGER || false
+  })
+
+  fastify.register(FastifySecretsHashiCorp, {
+    secrets: {
+      test: {
+        name: SECRET_NAME,
+        key: SECRET_KEY
+      }
+    },
+    clientOptions: {
+      vaultOptions: {
+        endpoint: 'http://127.0.0.1:8200'
+      },
+      mountPoint: MOUNT_POINT.V2
     }
   })
 
